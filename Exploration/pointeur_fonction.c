82df60e986ec9db1796d080303e3ecfb0b35ebe0
Florentinus <cremwoodproduction@gmail.com>
2018-02-13 23:33:12 +0100

    pointeur fonction 1

--- Exploration/pointeur_fonction.c
+++ Exploration/pointeur_fonction.c
@@ -10,6 +10,21 @@ int fonction_2(int nbr)
     return (nbr*nbr);
 }
 
+void fonction_3(int n)
+{
+    printf("fonction 3 avec %d\n", n);
+}
+
+void fonction_4(int n)
+{
+    printf("fonction 4 avec %d\n", n);
+}
+
+void fonction_3et4(int n, void (*ptr_fonction)(int))
+{
+    (*ptr_fonction)(n);
+}
+
 int main(int argc, char const *argv[])
 {
     int * ptrint; // dois contenir une adresse de variable int
@@ -29,5 +44,15 @@ int main(int argc, char const *argv[])
     ptr_fonction1 = fonction_2;
     int carre = (*ptr_fonction1)(5);
     printf("%d\n", carre);
+
+    // tableau de pointeur de fonction; retour(*nom_tableau_pointeur[taille_tableau])(liste_arguments)
+    // tableau de pointeur seulement avec les memes prototypes
+
+    // retour pointeur dans une fonction; type_retour_pointee(* fonction_renvoi(arguments_renvoi))(arguments_pointee)
+    // typedef; typedef retour (* ptr_fonction)(arguments);
+
+    fonction_3et4(2, fonction_3);
+    fonction_3et4(5, fonction_4);
+    // fonctionne aussi avec typedef
     return 0;
 }
